1362b755826856c1e71a9c1f05ca146ebcfaf550
GustavoDwSantos <gustavos200116@gmail.com>
2023-06-08 01:56:44 -0300

    Melhorias nas funções do pre_processing e análise * feat: Melhoria nas funções do pre_processing para o uso de kernels personalizados * feat: função img2etq que recebe a imagem e retorna o recorte da etiqueta * feat: função etqextractbarcode recebe uma imagem e retorna o recorte do barcode e a planilha

--- img_cutter.py
+++ img_cutter.py
@@ -1,4 +1,5 @@
 import cv2
+from pre_processing import *
 
 def calcula_contornos(img):
     contornos, hierarquia = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -12,3 +13,29 @@ def img2recorte(contornos, img, prop_min, prop_max):
         if proporcao >=prop_min and proporcao <=prop_max:
             etiqueta = img[y:y+h, x:x+w]
             return etiqueta
+        
+
+def img2recorte_barcode(contornos, img, prop_min, prop_max):
+    for contorno in contornos:
+        x, y, w, h = cv2.boundingRect(contorno)
+        proporcao = float(w)/h
+        if proporcao >=prop_min and proporcao <= prop_max:
+            barcode = img[0:-1, x:x+w]
+            planilha = img[0:-1, 0:x]
+            return barcode, planilha
+
+def img2etq(img):
+    img_transformed = aplica_masc(img, kernel_tuple=(10,25))
+    etq = img2recorte(calcula_contornos(img_transformed), img, 0.1, 0.9)
+    return etq
+
+def etqextractbarcode(img):
+    kernel_quad = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+    etq = img2etq(img)
+    img_blur = img2sobelblur(etq, kernel_tuple=(1,6))
+    limiarizacao = limiar_otsu(img_blur)
+
+    limiarizacao = cv2.erode(limiarizacao, kernel_quad, iterations = 2)
+    limiarizacao = cv2.dilate(limiarizacao, kernel_quad, iterations = 3)
+    barcode, planilha = img2recorte_barcode(calcula_contornos(limiarizacao), etq, 0.1, 0.9)
+    return barcode ,planilha
--- pre_processing.py
+++ pre_processing.py
@@ -1,23 +1,23 @@
 import cv2
 import numpy as np
 
-def img2blackhat(img):
+def img2blackhat(img,kernel_tuple):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    kernel_ret = cv2.getStructuringElement(cv2.MORPH_RECT, (10,25))
+    kernel_ret = cv2.getStructuringElement(cv2.MORPH_RECT, kernel_tuple)
     black_hat = cv2.morphologyEx(img, cv2.MORPH_BLACKHAT, kernel_ret)
     return black_hat
 
 
-def img2sobel(img):
-    img = img2blackhat(img)
+def img2sobel(img,kernel_tuple):
+    img = img2blackhat(img,kernel_tuple)
     sobel_y = cv2.Sobel(img, ddepth = cv2.CV_32F, dx = 0, dy = 1, ksize = 1)
     sobel_y = np.absolute(sobel_y)
     sobel_y = sobel_y.astype('uint8')
     return sobel_y
 
-def img2sobelblur(img):
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10,25))
-    img = img2sobel(img)
+def img2sobelblur(img,kernel_tuple):
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, kernel_tuple)
+    img = img2sobel(img,kernel_tuple)
     img = cv2.GaussianBlur(img,(5,5),0)
     img = cv2.morphologyEx(img, cv2.MORPH_CLOSE,kernel)
     return img
@@ -34,10 +34,10 @@ def img2mask(img):
     valor, mascara = cv2.threshold(fechamento, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     return mascara
 
-def aplica_masc(img):
+def aplica_masc(img,kernel_tuple):
     kernel_quad = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
 
-    img_blur = img2sobelblur(img)
+    img_blur = img2sobelblur(img,kernel_tuple)
     limiarizacao = limiar_otsu(img_blur)
 
     limiarizacao = cv2.erode(limiarizacao, kernel_quad, iterations = 2)
