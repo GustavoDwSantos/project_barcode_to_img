58a3d76d08482d624cebf83012bc09f2b8586bc0
GustavoDwSantos <gustavos200116@gmail.com>
2023-06-10 03:24:44 -0300

    Atualização da interface gráfica * feat: Adição de caixas de texto para exibir os dados obtidos * feat: Adição de labels para identificar os campos de dados * refactor: Remoção da função do botão, agora o botão está inativo * refactor: Remoção da segunda imagem de retorno * refactor: Alteração na classe App, ajustando o tamanho da janela

--- main.py
+++ main.py
@@ -16,15 +16,38 @@ class ScanFrame(Frame):
         #cam IP config
         self.url = "http://192.168.0.101:8080/shot.jpg"
         self.label_cam = Label(self)
-        self.label_cam.grid(row=1, column=0, **options)
+        self.label_cam.grid(row=1, rowspan=7, column=0, **options)
 
         
-        self.image_return = Label(self)
-        self.image_return.grid(row=1, column=2, **options)
+        """self.image_return = Label(self)
+        self.image_return.grid(row=1, column=2, **options)"""
+
+        # Entry Setting
+        self.lote_var = StringVar()
+        self.nom_wt_var = StringVar()
+        self.units_var = StringVar()
+        self.net_var = StringVar()
+        self.set_var = StringVar()
+        self.pkg_var = StringVar()
+
+        self.entries = [
+            ("Lote", self.lote_var),
+            ("NOM.WT", self.nom_wt_var),
+            ("UNITS", self.units_var),
+            ("NET", self.net_var),
+            ("SET", self.set_var),
+            ("PKG", self.pkg_var),
+        ]
+
+        for i, (label_text, var) in enumerate(self.entries):
+            self.label = Label(self, text=label_text)
+            self.label.grid(row=i+1, column=1, **options)
+            self.entry = Entry(self, textvariable=var, state="disabled")
+            self.entry.grid(row=i+1, column=2, **options)
 
         #Button Setting
         self.button = Button(self, text="Scan", command=self.Scan)
-        self.button.grid(row=2, column=1, **options)
+        self.button.grid(row=7, column=1, columnspan=2, **options)
 
         self.show_frames()
 
@@ -40,18 +63,19 @@ class ScanFrame(Frame):
         self.label_cam.after(10, self.show_frames)
     
     def Scan(self):
-        img = url2img(self.url)
+        pass
+        """img = url2img(self.url)
         img = Image.fromarray(img)
         imgtk = ImageTk.PhotoImage(image=img)
         self.image_return.imgtk = imgtk
-        self.image_return.configure(image=imgtk)
+        self.image_return.configure(image=imgtk)"""
 
 class App(Tk):
     def __init__(self):
         super().__init__()
 
         self.title("Barcode Reader")
-        self.geometry("1350x800")
+        self.geometry("900x560")
         self.resizable(True, True)
 
 
